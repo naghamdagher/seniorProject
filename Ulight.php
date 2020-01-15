95181b31ff8cecc94363a415f2bd896f08106c6b
naghamdagher <dagher_nagham1997@gmail.com>
2020-01-15 21:10:12 +0200

    content filling

--- Ulight.php
+++ Ulight.php
@@ -15,23 +15,23 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
       <div class="container">
   
-        <a class="navbar-brand js-scroll-trigger" href="#page-top">U-light </a>
+        <a class="navbar-brand js-scroll-trigger" href="#top">U-light </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="#about">Projects</a>
+              <a class="nav-link js-scroll-trigger" href="#projects">Projects</a>
             </li>
             <li class="nav-item">
               <a class="nav-link js-scroll-trigger" href="#services">Services</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="#contact">Contact us</a>
+              <a class="nav-link js-scroll-trigger" href="#contactus">Contact us</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="Login.html">Login</a>
+              <a class="nav-link js-scroll-trigger" href="Login.php">Login</a>
             </li>
           </ul>
         </div>
@@ -43,10 +43,10 @@
     <!--All text should be written in a paragraph to make the search function easy-->
 
  
-    <header class="bg-dark text-white">
-      <div class="container text-center">
+    <header class="bg-dark text-white" >
+      <div  class="container text-center" >
         <h1>Welcome to U-light</h1>
-        <p class="lead">Welcome to U-light</p>
+        <p class="lead">Brighten up your lifestyle</p>
       </div>
     </header>
 
@@ -60,14 +60,26 @@
       <div class="container">
         <div class="row">
           <div class="col-lg-8 mx-auto">
-            <p style="font-size:64px; color: orange; font-weight: bold;">About this page</p>
-            <p class="lead">This is a great place to talk about your webpage. This template is purposefully unstyled so you can use it as a boilerplate or starting point for you own landing page designs! This template features:</p>
-            <ul>
-              <li>Clickable nav links that smooth scroll to page sections</li>
-              <li>Responsive behavior when clicking nav links perfect for a one page website</li>
-              <li>Bootstrap's scrollspy feature which highlights which section of the page you're on in the navbar</li>
-              <li>Minimal custom CSS so you are free to explore your own unique design options</li>
-            </ul>
+            <p style="font-size:64px; color: orange; font-weight: bold;">What we do</p>
+            <p class="lead">Leading our entrepreneurs to start their own businesses, we help them and guide them 
+              to engage in particular work criteria to meet the requirements of their certificate. U-light SAL is 
+              a consortium of three companies whose core business lies in management of public utilities, 
+              serving the Middle East and Arab countries (GULF) region. U-light is the electric Distribution Service Provider 
+              (DSP) for Beirut Municipality, Mount Lebanon area, Batroun, Jbeil, Tripoli, Jezzine, and Bekaa service areas. 
+              The main goals of U-light is to:
+              <ul>
+                <li>Provide Customer Service to EDL customers</li>
+                <li>Rehabilitate the Electric Network in Lebanon</li>
+                <li>Install Electric Meters and advanced infrastructure</li>
+                <li>Increase the Capacity of the Network</li>
+                <li>Perform Maintenance and Operations on Behalf of EDL</li>
+                <li>Manage subscriptions, maintenance, emergency response</li>
+                <li>Construction of new distribution facilities</li>
+                <li>Planning, Programs and design</li>
+                <li>Network Survey </li>
+                <li>Meter Reading & Bill Collection </li>
+              </ul>
+           </p>
           </div>
         </div>
       </div>
@@ -77,57 +89,70 @@
 
     <!-- Image Showcases -->
   <section class="showcase">
-    <div class="container-fluid p-0">
+    <div class="container-fluid p-3">
       <div class="row no-gutters">
 
-        <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('img/bg-showcase-1.jpg');"></div>
+        <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('Images/workers.jpg');"></div>
         <div class="col-lg-6 order-lg-1 my-auto showcase-text">
-          <h2>Fully Responsive Design</h2>
-          <p class="lead mb-0">When you use a theme created by Start Bootstrap, you know that the theme will look great on any device, whether it's a phone, tablet, or desktop the page will behave responsively!</p>
+          <h2>Our team ready at your service</h2>
+          <p class="lead mb-0">
+            U-light Utility Services is a subsidiary of Dagher Co, created to design, 
+            implement, operate and maintain the electrical distribution network along with customer and 
+            metering services on behalf of Electricité du Liban. Its operations cover around 40% of the 
+            Lebanese territory going from northern part of Greater Beirut till northern borders. 
+            With over 800 employees, including 35 engineers, 200 technicians of all levels, 300 meter 
+            readers and bill collectors and 40 customer services specialists, U-light fulfills its activities 
+            based on defined Key Performance Indicators (KPI) related to the quality and efficiency of its 
+            various services.
+          </p>
         </div>
       </div>
       <div class="row no-gutters">
-        <div class="col-lg-6 text-white showcase-img" style="background-image: url('img/bg-showcase-2.jpg');"></div>
+        <div class="col-lg-6 text-white showcase-img" style="background-image: url('Images/locations.jpg');"></div>
         <div class="col-lg-6 my-auto showcase-text">
-          <h2>Updated For Bootstrap 4</h2>
-          <p class="lead mb-0">Newly improved, and full of great utility classes, Bootstrap 4 is leading the way in mobile responsive web development! All of the themes on Start Bootstrap are now using Bootstrap 4!</p>
+          <h2>Geographical distribution</h2>
+          <p class="lead mb-0">
+            Newly improved, and full of great utility classes, U-light is leading 
+            the way in electrical service! U-light is present on all the area it covers through its: 
+            Head-office in Bteghrine region for management, administration and planning. 
+            14 contact centers located in Antelias, Jounieh, Bikfaya, Baabdat, Amchit, Batroun, 
+            Achkout, Qartaba, Amioun, Zgharta, Tripoli, Donniyeh, Halba, Qobayat, Jezzine and Chouf. 
+            Selaata facility consisted of the 24/7 call center, the smart meters’ laboratory, 
+            the warehouse and the training center (including the pedagogic network) necessary 
+            for the employees’ continuous professional development.</p>
         </div>
       </div>
       <div class="row no-gutters">
-        <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('img/bg-showcase-3.jpg');"></div>
+        <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('Images/manOnPole1.jpg');"></div>
         <div class="col-lg-6 order-lg-1 my-auto showcase-text">
-          <h2>Easy to Use &amp; Customize</h2>
-          <p class="lead mb-0">Landing Page is just HTML and CSS with a splash of SCSS for users who demand some deeper customization options. Out of the box, just add your content and images, and your new landing page will be ready to go!</p>
-        </div>
-      </div>
-    </div>
-  </section>
-
-     <!-- Testimonials -->
-  <section class="testimonials text-center bg-light">
-    <div class="container">
-      <h2 class="mb-5">What people are saying...</h2>
-      <div class="row">
-        <div class="col-lg-4">
-          <div class="testimonial-item mx-auto mb-5 mb-lg-0">
-            <img class="img-fluid rounded-circle mb-3" src="img/testimonials-1.jpg" alt="">
-            <h5>Margaret E.</h5>
-            <p class="font-weight-light mb-0">"This is fantastic! Thanks so much guys!"</p>
-          </div>
-        </div>
-        <div class="col-lg-4">
-          <div class="testimonial-item mx-auto mb-5 mb-lg-0">
-            <img class="img-fluid rounded-circle mb-3" src="img/testimonials-2.jpg" alt="">
-            <h5>Fred S.</h5>
-            <p class="font-weight-light mb-0">"Bootstrap is amazing. I've been using it to create lots of super nice landing pages."</p>
-          </div>
-        </div>
-        <div class="col-lg-4">
-          <div class="testimonial-item mx-auto mb-5 mb-lg-0">
-            <img class="img-fluid rounded-circle mb-3" src="img/testimonials-3.jpg" alt="">
-            <h5>Sarah W.</h5>
-            <p class="font-weight-light mb-0">"Thanks so much for making these free resources available to us!"</p>
-          </div>
+          <h2>Operations &amp; Maintenance</h2>
+          <p class="lead mb-0">
+            <ul>
+              <li>
+              Repairing and maintaining the MV and LV distribution network that suffers from faults due 
+              to old or inappropriate equipment, weather, or human intervention.
+              </li>
+              <li>
+              Enhancing the electrical network’s used equipment to reduce technical losses and to ensure 
+              sustainability and high level of security (Preventive Maintenance).
+              </li>
+              <li>
+              Ensuring that the interruption of electricity supply to the customer is as brief as possible 
+              (excluding planned interruptions due to insufficient electricity generation which is beyond 
+              U-light’s responsibility).
+              </li>
+              <li>
+                Improving response time to customer-related operational and emergency requests.
+              </li>
+              <li>
+                Ensuring the safety of electricians and repairmen via personal safety suits, protection equipment, 
+                and U-light certified tools.
+              </li>
+              <li>
+                Training continuously the staff about electricity operations and safety instructions.
+              </li>
+            </ul>
+          </p>
         </div>
       </div>
     </div>
@@ -156,14 +181,80 @@
     <!--Division services-->
     <div id="services" class="container">
       <h1>Our services</h1>
+     <div>
+       <p>Key Figures:
+        3000 Km2  of covered Territory
+        520.000 Customers
+        2.000.000 Inhabitants
+        20.000 Km of Operated Network
+        6380 MV/LV Substations
+        850 Employees
+        16 Service Locations
+        130 Vehicles (including 5 bucket trucks and an underground fault detector)
+        55 M $US Network Investments expected until mid-2016   
+    </p>
+     </div> 
+
+     <div>
+       <p>Electricité Du Liban currently reports $300 million in annual losses, a significant 
+         part of which is attributable to electricity theft. This figure amounts to about 25% of 
+         Non-Technical Losses, while a 15% technical loss of electricity is reported due to the use 
+         of old and inadequate cables and components. Meter reading is also in need of improvements 
+         in this sector as the different types of meters which are given to various buildings are 
+         often subject to personal human intervention that derails the collective, general interest. 
+         It is in recognition of these problems that EDL has initiated the DSP project that aims to 
+         consolidate a number of tasks in a new contractual framework that ensures proper investment 
+         planning, effective execution of network extension, network operation and maintenance, metering 
+         and billing activities with full correlation of energy distributed, billed, and collected to 
+         minimize and eliminate losses. 
+         BUS was accordingly commissioned to maintain and operate the electricity distribution grid in 
+         Northern Mount Lebanon and North Lebanon.
+         BUS is responsible for providing a distribution service to customers that is as smooth and 
+         undisrupted as possible but is in no way responsible for Lebanon’s insufficient electricity 
+         production that results in the need to cut power in certain areas in accordance with the EDL plan
+        </p>
+     </div>
     </div>
+
+       <!-- Testimonials -->
+  <section class="testimonials text-center bg-light">
+    <div class="container">
+      <h2 class="mb-5">What people are saying...</h2>
+      <div class="row">
+        <div class="col-lg-4">
+          <div class="testimonial-item mx-auto mb-5 mb-lg-0">
+            <img class="img-fluid rounded-circle mb-3" src="img/testimonials-1.jpg" alt="">
+            <h5>Margaret E.</h5>
+            <p class="font-weight-light mb-0">"This is fantastic! Thanks so much guys!"</p>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="testimonial-item mx-auto mb-5 mb-lg-0">
+            <img class="img-fluid rounded-circle mb-3" src="img/testimonials-2.jpg" alt="">
+            <h5>Fred S.</h5>
+            <p class="font-weight-light mb-0">"Bootstrap is amazing. I've been using it to create lots of super nice landing pages."</p>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="testimonial-item mx-auto mb-5 mb-lg-0">
+            <img class="img-fluid rounded-circle mb-3" src="img/testimonials-3.jpg" alt="">
+            <h5>Sarah W.</h5>
+            <p class="font-weight-light mb-0">"Thanks so much for making these free resources available to us!"</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
     
-    <!--Division contact us-->
-    <div id="contactus" class="container">
+    <!--Division contact us and map in section-->
+    <div id="contactus" class="container" style="display: block; ">
       <h1>Contact us</h1>
+     <div>
 
-    </div>
-
+     </div>
+   
+    <div id="map"></div>
+ </div>
     <div class="footer">
     
       <div class="footer-copyright text-center py-3">
@@ -195,10 +286,12 @@
                }
        }
     });
+   
     </script>
     <script src="scriptfiles/bootstrap.min.js"></script>
     <script src="scriptfiles/jquery-3.3.1.min.js"></script>
     <script src="scriptfiles/script.js"></script>
+ 
 </body>
 
 
